fa11ae9735f489a39a6b21162462a3de5871838e
Ngoc Le <ngocle266tqt@gmail.com>
2025-06-16 16:07:24 +0700

    Add fallback for get request

--- spring-petclinic-api-gateway/src/main/java/org/springframework/samples/petclinic/api/boundary/web/FallbackController.java
+++ spring-petclinic-api-gateway/src/main/java/org/springframework/samples/petclinic/api/boundary/web/FallbackController.java
@@ -2,6 +2,7 @@ package org.springframework.samples.petclinic.api.boundary.web;
 
 import org.apache.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class FallbackController {
 
     @PostMapping("/fallback")
-    public ResponseEntity<String> fallback() {
+    public ResponseEntity<String> fallbackPost() {
         return ResponseEntity.status(HttpStatus.SC_SERVICE_UNAVAILABLE)
                 .body("Chat is currently unavailable. Please try again later.");
+    }
+
+    @GetMapping("/fallback")
+    public ResponseEntity<String> fallbackGet() {
+        return ResponseEntity.status(HttpStatus.SC_SERVICE_UNAVAILABLE)
+                .body("Service is currently unavailable. Please try again later.");
     }
 }
